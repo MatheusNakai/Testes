899c151cba1f31dd642168a5bed98b9cf2b7f95e
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 17:01:15 -0300

    Mudanca na classe User

--- Python/Discord_bot/classes/User.py
+++ Python/Discord_bot/classes/User.py
@@ -2,14 +2,6 @@ class User:
     def __init__(self, id, name, lvl, exp):
         self.id = id
         self.name = name
-        self.lvl = lvl
-        self.exp = exp
 
     def __str__(self):
         return 'User: {}'.format(self.name)
-
-    def addExp(self, exp):
-        self.exp += exp
-
-    def addLvl(self, lvl):
-        self.lvl += lvl
