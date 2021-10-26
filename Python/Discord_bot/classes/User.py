aaba0a46fa566a2f6155ec2341f6a36baa355f62
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 16:50:21 -0300

    Criacao da classe User e seu contrutor

---
+++ Python/Discord_bot/classes/User.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, id, name, lvl, exp):
+        self.id = id
+        self.name = name
+        self.lvl = lvl
+        self.exp = exp
+
+    def __str__(self):
+        return 'User: {}'.format(self.name)
