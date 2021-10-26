76e4e2f9dc8379bdbb7614b29ee2bca14169a25f
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 16:28:35 -0300

    Criacao da funcao bubble_sort

--- Python/GitHub_CoPilot/SortAlgorithm/Sort.py
+++ Python/GitHub_CoPilot/SortAlgorithm/Sort.py
@@ -7,4 +7,10 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
     return arr
-    
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
