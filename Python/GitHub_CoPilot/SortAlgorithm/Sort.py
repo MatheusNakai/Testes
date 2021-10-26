977f99049734bf643522a304217bce68d1420fb2
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 16:29:24 -0300

    criacao da funcao selection_sort

--- Python/GitHub_CoPilot/SortAlgorithm/Sort.py
+++ Python/GitHub_CoPilot/SortAlgorithm/Sort.py
@@ -14,3 +14,12 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
