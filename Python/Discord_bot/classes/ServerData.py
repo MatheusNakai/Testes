e1ffa11a7cf65c445ccd8421695346b086dc5ae6
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 16:59:44 -0300

    Criacao da classe ServerData e seu construtor

---
+++ Python/Discord_bot/classes/ServerData.py
@@ -0,0 +1,7 @@
+class ServerData:
+    def __init__(self, id, server_id, user_id, exp, lvl):
+        self.id = id
+        self.server_id = server_id
+        self.user_id = user_id
+        self.exp = exp
+        self.lvl = lvl
