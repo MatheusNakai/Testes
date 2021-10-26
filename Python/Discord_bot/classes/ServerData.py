1ef30911fa00d4f9bd35b4a5e52e358e99cfd485
MatheusNakai <matheusnakai@hotmail.com>
2021-10-26 17:02:07 -0300

    Update ServerData.py

--- Python/Discord_bot/classes/ServerData.py
+++ Python/Discord_bot/classes/ServerData.py
@@ -1,7 +1,7 @@
 class ServerData:
-    def __init__(self, id, server_id, user_id, exp, lvl):
+    def __init__(self, id, server_id, user_id):
         self.id = id
         self.server_id = server_id
         self.user_id = user_id
-        self.exp = exp
-        self.lvl = lvl
+        self.exp = 0
+        self.lvl = 1
