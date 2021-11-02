b04c8a812123d5f0033d187ec3fafce8e9f425d7
MatheusNakai <matheusnakai@hotmail.com>
2021-11-02 20:00:40 -0300

    Revendo Teste em Python

---
+++ Python/Coursera/UniTest.py
@@ -0,0 +1,8 @@
+from Regex import rearrenge_name
+import unittest
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrenge_name(testcase), expected)
