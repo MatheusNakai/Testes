271a9b59e27630bd186d4875e5183b492f5b24f8
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-02-24 10:19:03 -0300

    update

--- Python/Instagram/ProfilePicDownload.py
+++ Python/Instagram/ProfilePicDownload.py
@@ -6,6 +6,7 @@ def download(username):
     instance = instaloader.Instaloader()
 
     os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
+    instance.download_profile(username)
     return instance.download_profile(username, profile_pic_only=True)
 
 if __name__ == '__main__':
