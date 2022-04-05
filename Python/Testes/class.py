7b2f00eb793a0939e1e854becbe994396891191c
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-05 13:42:14 -0300

    Learning
    Transform a class to a dictionary

---
+++ Python/Testes/class.py
@@ -0,0 +1,12 @@
+class Teste:
+    soma = 0
+    te=0
+    def __init__(self, valor):
+        self.soma += valor
+        self.te=0
+    def __str__(self):
+        return str(self.valor)
+    
+t1 = Teste(1)
+
+print(t1.__dict__)
